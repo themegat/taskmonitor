403391068e1469323c736cbfa8752365036581ce
Tshepo Motsoeneng <tshepomotsoeneng0@gmail.com>
2018-04-11 16:07:46 +0200

    Commit 11-04-2018 16:07

--- js/view.js
+++ js/view.js
@@ -1,7 +1,6 @@
-// let $ = require('jquery');
+
 const remote = require('electron').remote;
 var win = remote.getCurrentWindow();
-var isAppCollapsed = false;
 var appInitHieght = win.getSize()[1];
 
 $('#headTest').text("this is a test");
@@ -12,20 +11,34 @@ $('#example1').calendar();
 $('#btnCloseApp').on("click", function () {
     win.close();
 });
-$('#btnResizeApp').on("click", function () {
-    if (!isAppCollapsed) {
+
+//Control the view state of the application (collapse view)
+
+var AppViewState = function(){
+    this.isAppCollapsed = false;
+};
+
+AppViewState.prototype.toggleCollapse = function(){
+    if (!this.isAppCollapsed) {
         $('#rowBody').hide();
         $('body').css('overflow-y', 'hidden');
         $("#btnResizeApp_icon").removeClass("up");
         $('#btnResizeApp_icon').addClass("down");
         win.setSize(win.getSize()[0], 50);
-        isAppCollapsed = true;
+        this.isAppCollapsed = true;
     } else {
         $('#rowBody').show();
         $('body').css('overflow-y', 'auto');
         $("#btnResizeApp_icon").removeClass("down");
         $('#btnResizeApp_icon').addClass("up");
         win.setSize(win.getSize()[0], appInitHieght);
-        isAppCollapsed = false;
+        this.isAppCollapsed = false;
     }
+};
+
+var appState = new AppViewState();
+$('#btnResizeApp').on("click", function () {
+    appState.toggleCollapse();
 });
+
+//End control state
--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const electron = require('electron');
 const {app , BrowserWindow} = require('electron');
 const path = require('path');
 const url = require('url');
 
 function createWindow(){
+    var appWidth = 350;
+    var screenWidth = electron.screen.getPrimaryDisplay().workAreaSize.width
     let win = new BrowserWindow({
         width: 350, height: 450, 
-        x: 1000, y: 40, minimizable: false, maximizable: false,
+        x: (screenWidth - appWidth), y: 40, minimizable: false, maximizable: false,
         alwaysOnTop: true,
         frame: false,
         resizable: false,
